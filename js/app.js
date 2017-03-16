9c41d93ee347a2a423436dea5bd7e5d70b519ecc
jake_ryan <jaker@getfluid.com>
2017-03-16 14:57:07 -0600

    this

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 $(document).foundation();
 
-
-$(document).ready(function() {
-  //menu button toggle
-  $('.menu-toggle').click(function() {
-    $('header').toggleClass('active');
-  });
+//menu button toggle
+$('.menu-toggle').click(function() {
+  $('header').toggleClass('active');
 });
