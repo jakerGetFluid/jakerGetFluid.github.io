9e8dad55816adde5b9273328479a5ded3a5f2d5c
jake_ryan <jaker@getfluid.com>
2017-03-16 14:44:57 -0600

    redesign

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+
+$(document).ready(function() {
+  // menu width, set off screen offset
+  var width = $('aside.off-menu').css('width');
+  $('aside.off-menu').css('left', '-'+width);
+  //menu button toggle
+  $('.menu-toggle').click(function() {
+    $('header').toggleClass('active');
+  });
+});
