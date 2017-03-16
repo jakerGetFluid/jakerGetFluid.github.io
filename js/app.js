0ac320b744a040cb7355c266165b0921c23459db
jake_ryan <jaker@getfluid.com>
2017-03-16 14:51:11 -0600

    menu toggle simplify

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@ $(document).foundation();
 
 
 $(document).ready(function() {
-  // menu width, set off screen offset
-  var width = $('aside.off-menu').css('width');
-  $('aside.off-menu').css('left', '-'+width);
   //menu button toggle
   $('.menu-toggle').click(function() {
     $('header').toggleClass('active');
