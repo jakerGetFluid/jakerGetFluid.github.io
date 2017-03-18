13cd5b543aa6c02d7b10960c4eb738c1ade31999
jake_ryan <jaker@getfluid.com>
2017-03-17 21:59:05 -0600

    fade bg after scroll

--- js/app.js
+++ js/app.js
@@ -5,4 +5,17 @@ $('.menu-toggle').click(function() {
   $('header').toggleClass('active');
 });
 
+//fade title in/ out
 $('.hero .title').headroom();
+
+// waypoints fades
+$('section.about').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('.hero').addClass('fade-out');
+    }
+    else {
+      $('.hero').removeClass('fade-out');
+    }
+  }, {
+    offset: '20%'
+  });
