5ff49a1b308b81d15303f3bda4f2aa1bdd8a3bdc
jake_ryan <jaker@getfluid.com>
2017-03-20 21:39:08 -0600

    this

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ $('.menu-toggle').click(function() {
   $('header').toggleClass('active');
 });
 
+//smooth scroll
+$('.ss').click(function() {
+  if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({scrollTop: target.offset().top}, 1000); // Set scroll speed here
+      return false;
+    }
+  }
+});
+
 //fade title in/ out
 $('.hero .title').headroom();
 
@@ -19,3 +31,14 @@ $('section.about').waypoint(function(direction) {
   }, {
     offset: '20%'
   });
+
+$('a[name="about"]').waypoint(function(direction) {
+  if (direction == 'down') {
+    $('section.about').addClass('fade-in');
+  }
+  else {
+    $('section.about').removeClass('fade-in');
+  }
+  }, {
+    offset: '55%'
+});
