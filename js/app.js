d6434ed94f2d6040e8c1684f3a592920926633cc
jake_ryan <jaker@getfluid.com>
2017-03-27 16:26:38 -0600

    this

--- js/app.js
+++ js/app.js
@@ -27,28 +27,42 @@ $('.hero .title').headroom();
 // waypoints fades
 // hero section fade out, about fade in
 $('section.about').waypoint(function(direction) {
-    if (direction == 'down') {
-      $('.hero').addClass('fade-out');
-      $('section.about').addClass('fade-in');
-    }
-    else {
-      $('.hero').removeClass('fade-out');
-      $('section.about').removeClass('fade-in');
-    }
-  }, {
-    offset: '20%'
-  });
+  if (direction == 'down') {
+    $('.hero').addClass('fade-out');
+    $('section.about').addClass('fade-in');
+  }
+  else {
+    $('.hero').removeClass('fade-out');
+    $('section.about').removeClass('fade-in');
+  }
+}, {
+  offset: '20%'
+});
 
-  //about fade out, skillset fade in
-  $('section.skillset').waypoint(function(direction) {
-      if (direction == 'down') {
-        $('section.about').removeClass('fade-in');
-        $('section.skillset').addClass('fade-in');
-      }
-      else {
-        $('section.about').addClass('fade-in');
-        $('section.skillset').removeClass('fade-in')
-      }
-    }, {
-      offset: '20%'
-    });
+//about fade out, skillset fade in
+$('section.skillset').waypoint(function(direction) {
+  if (direction == 'down') {
+    $('section.about').removeClass('fade-in');
+    $('section.skillset').addClass('fade-in');
+  }
+  else {
+    $('section.about').addClass('fade-in');
+    $('section.skillset').removeClass('fade-in')
+  }
+}, {
+  offset: '20%'
+});
+
+//skillset fade out, samples fade in
+$('section.samples').waypoint(function(direction) {
+  if (direction == 'down') {
+    $('section.skillset').removeClass('fade-in');
+    $('section.samples').addClass('fade-in');
+  }
+  else {
+    $('section.skillset').addClass('fade-in');
+    $('section.samples').removeClass('fade-in')
+  }
+}, {
+  offset: '20%'
+});
